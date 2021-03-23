386917cb3d3c98e71a5486bde496633fcf5b27ac
David Baik <ygbaik6@gmail.com>
2021-03-23 16:34:45 -0700

    Screw you carousels

--- js/carousel.js
+++ js/carousel.js
@@ -1,129 +1,129 @@
 'use stict';
 
-var slider = document.getElementById('slider'),
-    sliderItems = document.getElementById('items'),
-    prev = document.getElementById('prev'),
-    next = document.getElementById('next');
+// CREDIT to https://codepen.io/carlosalvarez91/pen/Mowdyv
+// Copyright (c) 2021 by Carlos Álvarez (https://codepen.io/carlosalvarez91/pen/Mowdyv)
 
-slide(slider, sliderItems, prev, next);
+//CAROUSEL SETTINGS
+var $imagesCarousel = jQuery('.carousel-items').flickity({
+  /* options, defaults listed */
 
-function slide(wrapper, items, prev, next) {
-  var posX1 = 0,
-      posX2 = 0,
-      posInitial,
-      posFinal,
-      threshold = 100,
-      slides = items.getElementsByClassName('social-media-card-wrapper'),
-      slidesLength = slides.length,
-      slideSize = items.getElementsByClassName('social-media-card-wrapper')[0].offsetWidth,
-      firstSlide = slides[0],
-      lastSlide = slides[slidesLength - 1],
-      cloneFirst = firstSlide.cloneNode(true),
-      cloneLast = lastSlide.cloneNode(true),
-      index = 0,
-      allowShift = true;
-  
-  // Clone first and last slide
-  items.appendChild(cloneFirst);
-  items.insertBefore(cloneLast, firstSlide);
-  wrapper.classList.add('loaded');
-  
-  // Mouse and Touch events
-  items.onmousedown = dragStart;
-  
-  // Touch events
-  items.addEventListener('touchstart', dragStart);
-  items.addEventListener('touchend', dragEnd);
-  items.addEventListener('touchmove', dragAction);
-  
-  // Click events
-  prev.addEventListener('click', function () { shiftSlide(-1) });
-  next.addEventListener('click', function () { shiftSlide(1) });
-  
-  // Transition events
-  items.addEventListener('transitionend', checkIndex);
-  
-  function dragStart (e) {
-    e = e || window.event;
-    e.preventDefault();
-    posInitial = items.offsetLeft;
-    
-    if (e.type == 'touchstart') {
-      posX1 = e.touches[0].clientX;
-    } else {
-      posX1 = e.clientX;
-      document.onmouseup = dragEnd;
-      document.onmousemove = dragAction;
-    }
+  accessibility: false,
+  /* enable keyboard navigation, pressing left & right keys */
+
+  autoPlay: false,
+  pauseAutoPlayOnHover: false,
+  /* advances to the next cell
+  if true, default is 3 seconds
+  or set time between advances in milliseconds
+  i.e. `autoPlay: 1000` will advance every 1 second */
+
+  cellAlign: 'center',
+  /* alignment of cells, 'center', 'left', or 'right'
+  or a decimal 0-1, 0 is beginning (left) of container, 1 is end (right) */
+
+  // cellSelector: '.topic-switcher__item',
+  /* specify selector for cell elements */
+
+  contain: false,
+  /* will contain cells to container
+  so no excess scroll at beginning or end
+  has no effect if wrapAround is enabled */
+
+  draggable: true,
+  /* enables dragging & flickin
+  freeScroll: false,
+  enables content to be freely scrolled and flicked
+  without aligning cells */
+
+  friction: 0.2,
+  /* smaller number = easier to flick farther */
+
+  initialIndex: 0,
+  /* zero-based index of the initial selected cell */
+
+  lazyLoad: false,
+  /* enable lazy-loading images
+  set img data-flickity-lazyload="src.jpg"
+  set to number to load images adjacent cells */
+
+  percentPosition: true,
+  /* sets positioning in percent values, rather than pixels
+  Enable if items have percent widths
+  Disable if items have pixel widths, like images */
+
+  prevNextButtons: false,
+  /* creates and enables buttons to click to previous & next cells */
+
+  pageDots: false,
+  /* create and enable page dots */
+
+  resize: true,
+  /* listens to window resize events to adjust size & positions */
+
+  rightToLeft: false,
+  /* enables right-to-left layout */
+
+  setGallerySize: true,
+  /* sets the height of gallery
+  disable if gallery already has height set with CSS */
+
+  watchCSS: false,
+  /* watches the content of :after of the element
+  activates if #element:after { content: 'flickity' }
+  IE8 and Android 2.3 do not support watching :after
+  set watch: 'fallbackOn' to enable for these browsers */
+
+  wrapAround: true
+    /* at end of cells, wraps-around to first for infinite scrolling */
+});
+
+function resizeCells() {
+  var flkty = $imagesCarousel.data('flickity');
+  var $current = flkty.selectedIndex;
+  var $length = flkty.cells.length;
+  var $imageNumLimit = 5;
+  if ($length < $imageNumLimit) {
+    $imagesCarousel.flickity('destroy');
   }
-
-  function dragAction (e) {
-    e = e || window.event;
-    
-    if (e.type == 'touchmove') {
-      posX2 = posX1 - e.touches[0].clientX;
-      posX1 = e.touches[0].clientX;
-    } else {
-      posX2 = posX1 - e.clientX;
-      posX1 = e.clientX;
-    }
-    items.style.left = (items.offsetLeft - posX2) + "px";
+  jQuery('.carousel-items .carousel-post').removeClass("nextToSelectedLeft");
+  jQuery('.carousel-items .carousel-post').removeClass("nextToSelectedRight");
+  jQuery('.carousel-items .carousel-post').removeClass("nextToSelectedLeft2");
+  jQuery('.carousel-items .carousel-post').removeClass("nextToSelectedRight2");
+  jQuery('.carousel-items .carousel-post').eq($current - 1).addClass("nextToSelectedLeft");
+  jQuery('.carousel-items .carousel-post').eq($current - 2).addClass("nextToSelectedLeft2");
+  var $endCell;
+  if ($current + 1 == $length) {
+    $endCell = "0";
+  } else {
+    $endCell = $current + 1;
   }
-  
-  function dragEnd (e) {
-    posFinal = items.offsetLeft;
-    if (posFinal - posInitial < -threshold) {
-      shiftSlide(1, 'drag');
-    } else if (posFinal - posInitial > threshold) {
-      shiftSlide(-1, 'drag');
-    } else {
-      items.style.left = (posInitial) + "px";
-    }
-
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-  
-  function shiftSlide(dir, action) {
-    items.classList.add('shifting');
-    
-    if (allowShift) {
-      if (!action) { posInitial = items.offsetLeft; }
-
-      if (dir == 1) {
-        items.style.left = (posInitial - slideSize) + "px";
-        index++;      
-      } else if (dir == -1) {
-        items.style.left = (posInitial + slideSize) + "px";
-        index--;      
-      }
-    };
-    
-    allowShift = false;
-  }
-    
-  function checkIndex (){
-    items.classList.remove('shifting');
-
-    if (index == -1) {
-      items.style.left = -(slidesLength * slideSize) + "px";
-      index = slidesLength - 1;
-    }
-
-    if (index == slidesLength) {
-      items.style.left = -(1 * slideSize) + "px";
-      index = 0;
-    }
-    
-    allowShift = true;
+  jQuery('..carousel-items .carousel-post').eq($endCell).addClass("nextToSelectedRight");
+  if($endCell + 1 < $imageNumLimit){
+    jQuery('.carousel-items .carousel-post').eq($endCell + 1).addClass("nextToSelectedRight2"); 
+  } else {
+    jQuery('.carousel-items .carousel-post').eq(0).addClass("nextToSelectedRight2");
   }
 }
+resizeCells();
 
-// const carouselContainer = document.querySelector('carousel-container').offsetWidth;
+$imagesCarousel.on('scroll.flickity', function() {
+  resizeCells();
+});
 
-// let index = 0;
 
-// next.addEventListener('click', () => {
-// 	index++;
-// 	slider.style.transform = 'translateX(-${index * carouselContainer}px)';
-// })
+//HOVER FUNCTIONS
+$('.carousel-post').bind("mouseover", function(){
+  if (this.className === 'carousel-post nextToSelectedLeft') {
+    $imagesCarousel.flickity('playLeftSlowPlayer');
+  } else if (this.className === 'carousel-post nextToSelectedLeft2') {
+    $imagesCarousel.flickity('playLeftFastPlayer');
+  } else if (this.className === 'carousel-post nextToSelectedRight') {
+    $imagesCarousel.flickity('playRightSlowPlayer');
+  } else if (this.className === 'carousel-post nextToSelectedRight2') {
+    $imagesCarousel.flickity('playRightFastPlayer');
+  }
+});
+
+$('.carousel-post').bind("mouseout", function(){
+  $imagesCarousel.flickity('pausePlayer');
+});
