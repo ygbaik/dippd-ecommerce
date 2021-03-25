a135ecf585cd0a79bdb7332d264477b531ca0a79
Joanna <jongeun0114@gmail.com>
2021-03-24 19:10:43 -0700

    Prices now have two decimal places and NaN issue is fixed

--- js/shop.js
+++ js/shop.js
@@ -5,9 +5,19 @@ let addButton = document.querySelectorAll('.add-button');
 let removeButton = document.querySelectorAll('.removeButton');
 
 let productNumbers = localStorage.getItem('boxNumbers');
-document.querySelector('.box-total span').textContent = productNumbers;
 let productPrices = localStorage.getItem('boxPrices');
-document.querySelector('.box-total-price span').textContent = productPrices;
+
+if (productNumbers && productPrices) {
+	document.querySelector('.box-total span').textContent = productNumbers;
+	document.querySelector('.box-total-price span').textContent = productPrices;
+} else {
+	localStorage.setItem('boxNumbers', 0);
+	localStorage.setItem('boxPrices', 0);
+
+	productNumbers = 0;
+	productPrices = 0;
+}
+
 
 for (let i=0; i < addButton.length; i++){
 	addButton[i].addEventListener('click', () => {
@@ -45,13 +55,13 @@ function boxPrices(){
 
 	if(productPrices > 0 && productPrices < 21){
 		localStorage.setItem('boxPrices', productPrices + 3.50);
-		document.querySelector('.box-total-price span').textContent = productPrices + 3.50;
+		document.querySelector('.box-total-price span').textContent = (Math.round((productPrices + 3.50) * 100) / 100).toFixed(2);
 	} else if (productPrices == 0){
 		localStorage.setItem('boxPrices', 3.50);
-		document.querySelector('.box-total-price span').textContent = 3.50;
+		document.querySelector('.box-total-price span').textContent = 3.50.toFixed(2);
 	} else{
 		localStorage.setItem('boxPrices', productPrices);
-		document.querySelector('.box-total-price span').textContent = productPrices;
+		document.querySelector('.box-total-price span').textContent = productPrices.toFixed(2);
 	}
 }
 
@@ -74,7 +84,7 @@ function removeBoxPrices(){
 
 	if(productPrices > 0){
 		localStorage.setItem('boxPrices', productPrices - 3.50);
-		document.querySelector('.box-total-price span').textContent = productPrices - 3.50;
+		document.querySelector('.box-total-price span').textContent = (Math.round((productPrices - 3.50) * 100) / 100).toFixed(2);
 	} else{
 		localStorage.setItem('boxPrices', 0);
 		document.querySelector('.box-total-price span').textContent = 0;
